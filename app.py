1fc0d041b68d5710b85e848ead4756e5d594b54a
MiguelCastiblanco <92052730+MiguelCastiblanco@users.noreply.github.com>
2021-10-12 22:55:01 -0500

    Modificaciones master

---
+++ app.py
@@ -0,0 +1,32 @@
+from logging import debug
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+@app.route('/')
+def Inicio():
+    return render_template ('login.html')
+
+@app.route('/Productos')
+def Productos():
+    return render_template ('panelProductos.html')
+
+@app.route('/Crear_Productos')
+def crearProductos():
+    return render_template ('producto.html')
+
+@app.route('/Usuarios')
+def Usuarios():
+    return render_template ('/panelUsuarios.html')
+
+@app.route('/Proveedores')
+def Proveedores():
+    return render_template ('panelProveedores.html')
+
+@app.route('/Crear_Proveedor')
+def crearProveedor():
+    return render_template ('proveedor.html')
+
+if __name__ == '__main__':
+    app.run (debug = True)
+
