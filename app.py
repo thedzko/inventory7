401a7f387cb02a0f74861a68164ea43c5d93683c
thedzko <maumerk2@gmail.com>
2021-10-21 20:47:09 -0500

    Flask CRUD

--- app.py
+++ app.py
@@ -1,32 +1,205 @@
-from logging import debug
-from flask import Flask, render_template, url_for
+from sqlite3.dbapi2 import connect
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import date, datetime
+# Importación SQLite
+import sqlite3
 
 app = Flask (__name__)
 
-@app.route('/')
-def Inicio():
-    return render_template ('login.html')
+# Conectar base de datos SQLite
+def sql_connection():
+    connectDB = sqlite3.connect('inventory.db')
+    return connectDB
+# Rutas y Funciones
 
-@app.route('/Productos')
-def Productos():
-    return render_template ('panelProductos.html')
+## USUARIOS
+# GET (LIST) - USER
+@app.route('/', methods=['GET'])
+def panelUsuarios():
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    cur.execute("SELECT * FROM users")
+    #Nueva variable para que traiga todo lo de la tabla
+    user_data = cur.fetchall()
+    return render_template('panelUsuarios.html', users = user_data)
 
-@app.route('/Crear_Productos')
-def crearProductos():
-    return render_template ('producto.html')
+# POST - USER
+@app.route('/newUser', methods=['GET','POST'])
+def crearUsuario():
+    if request.method == 'POST':
+        username = request.form['username']
+        role = request.form['role']
+        user_join = datetime.now()
+        user_age = datetime.now()
+        connectDB = sql_connection()
+        cur = connectDB.cursor()
+        statement = "INSERT INTO users (username, role, user_join, user_age) VALUES (?, ?, ?, ?)"
+        cur.execute(statement, [username, role, user_join, user_age])
+        connectDB.commit()
+        cur.close
+        return redirect(url_for('panelUsuarios'))
+    else:
+        return render_template('crearUsuario.html')
 
-@app.route('/Usuarios')
-def Usuarios():
-    return render_template ('/panelUsuarios.html')
+# GET(SHOW) - USER
+@app.route('/showuser/<int:user_id>', methods=['GET'])
+def verUsuario(user_id):
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    consulta = "SELECT * FROM users WHERE user_id=?"
+    cur = cur.execute(consulta, [user_id])
+    user_data = cur.fetchone()
+    cur.close
+    return render_template('verUsuario.html', users = user_data)
 
-@app.route('/Proveedores')
-def Proveedores():
-    return render_template ('panelProveedores.html')
+# EDIT - USER
+@app.route('/edituser/<int:user_id>', methods=['GET', 'POST'])
+def editarUsuario(user_id):
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    consulta = "SELECT * FROM users WHERE user_id=?"
+    cur = cur.execute(consulta, [user_id])
+    user_data = cur.fetchone()
+    cur.close
+    return render_template('editarUsuario.html', users = user_data)
 
-@app.route('/Crear_Proveedor')
+# UPDATE - USER
+@app.route('/updateuser/<int:user_id>', methods=['GET', 'POST'])
+def actualizarUsuario(user_id):
+    if request.method == 'POST':
+        connectDB = sql_connection()
+        username = request.form['username']
+        role = request.form['role']
+        cur = connectDB.cursor()
+        consulta = "UPDATE users SET username = ?, role = ? WHERE user_id = ?"
+        cur.execute(consulta, [username, role, user_id])
+        connectDB.commit()
+        cur.close
+        return redirect(url_for('panelUsuarios'))
+
+# DELETE - USER
+@app.route('/deleteuseruser/<int:user_id>', methods=['GET', 'POST'])
+def borrarUsuario(user_id):
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    cur.execute('DELETE FROM users WHERE user_id={0}'.format(user_id))
+    connectDB.commit()
+    return redirect(url_for('panelUsuarios'))
+
+
+## PRODUCTOS
+# GET (LIST) - PRODUCT
+@app.route('/productos/', methods=['GET'])
+def panelProductos():
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    cur.execute("SELECT * FROM products")
+    product_data = cur.fetchall()
+    return render_template('panelProductos.html', products = product_data)
+
+# POST - PRODUCT
+@app.route('/newProduct/', methods=['GET','POST'])
+def crearProducto():
+    if request.method == 'POST':
+        productname = request.form['productname']
+        providers = request.form['providers']
+        available_amount = request.form['available_amount']
+        least_amount = request.form['least_amount']
+        product_description = request.form['product_description']
+        product_retail = request.form['product_retail']
+        product_trade = request.form['product_trade']
+        connectDB = sql_connection()
+        cur = connectDB.cursor()
+        statement = "INSERT INTO products (productname, providers, available_amount, least_amount, product_description, product_retail, product_trade) VALUES (?, ?, ?, ?, ?, ?, ?)"
+        cur.execute(statement, [productname, providers, available_amount, least_amount, product_description, product_retail, product_trade])
+        connectDB.commit()
+        cur.close
+        return redirect(url_for('panelProductos'))
+    else:
+        return render_template('crearProducto.html')
+
+# GET(SHOW) - PRODUCT
+@app.route('/showproduct/<int:product_id>', methods=['GET'])
+def verProducto(product_id):
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    consulta = "SELECT * FROM products WHERE product_id=?"
+    cur = cur.execute(consulta, [product_id])
+    product_data = cur.fetchone()
+    cur.close
+    return render_template('verProducto.html', products = product_data)
+
+# EDIT - PRODUCT
+@app.route('/editproduct/<int:product_id>', methods=['GET', 'POST'])
+def editarProducto(product_id):
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    consulta = "SELECT * FROM products WHERE product_id=?"
+    cur = cur.execute(consulta, [product_id])
+    product_data = cur.fetchone()
+    cur.close
+    return render_template('editarProducto.html', products = product_data)
+
+# UPDATE - PRODUCT
+@app.route('/updateproduct/<int:product_id>', methods=['GET', 'POST'])
+def actualizarProducto(product_id):
+    if request.method == 'POST':
+        connectDB = sql_connection()
+        productname = request.form['productname']
+        providers = request.form['providers']
+        available_amount = request.form['available_amount']
+        least_amount = request.form['least_amount']
+        product_description = request.form['product_description']
+        product_retail = request.form['product_retail']
+        product_trade = request.form['product_trade']
+        cur = connectDB.cursor()
+        consulta = "UPDATE products SET productname = ?, providers = ?, available_amount = ?, least_amount = ?, product_description = ?, product_retail = ?, product_trade = ? WHERE product_id = ?"
+        cur.execute(consulta, [productname, providers, available_amount, least_amount, product_description, product_retail, product_trade, product_id])
+        connectDB.commit()
+        cur.close
+        return redirect(url_for('panelProductos'))
+
+# DELETE - PRODUCT
+@app.route('/deleteproduct/<int:product_id>/', methods=['GET', 'POST'])
+def borrarProducto(product_id):
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    cur.execute('DELETE FROM products WHERE product_id={0}'.format(product_id))
+    connectDB.commit()
+    return redirect(url_for('panelProductos'))
+
+
+## PROVEEDORES
+# GET (LIST) - PROVIDER
+@app.route('/proveedores/', methods=['GET'])
+def panelProveedores():
+    connectDB = sql_connection()
+    cur = connectDB.cursor()
+    cur.execute("SELECT * FROM providers")
+    provider_data = cur.fetchall()
+    return render_template('panelProveedores.html', providers = provider_data)
+
+# POST - PRODUCT
+@app.route('/newProvider/', methods=['GET','POST'])
 def crearProveedor():
-    return render_template ('proveedor.html')
+    if request.method == 'POST':
+        providername = request.form['providername']
+        products = request.form['products']
+        phone = request.form['phone']
+        celular = request.form['celular']
+        email = request.form['email']
+        location = request.form['location']        
+        connectDB = sql_connection()
+        cur = connectDB.cursor()
+        statement = "INSERT INTO products (providername, products, phone, celular, email, location VALUES (?, ?, ?, ?, ?, ?, ?)"
+        cur.execute(statement, [providername, products, phone, celular, email, location])
+        connectDB.commit()
+        cur.close
+        return redirect(url_for('panelProveedores'))
+    else:
+        return render_template('crearProveedor.html')
+
 
 if __name__ == '__main__':
-    app.run (debug = True)
-
+    app.debug = True
+    app.run()
